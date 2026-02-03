1707462ded9fe954f7a7ea700c59bf6c4e4119b4
PabloGMD <pablogmdsoftware@gmail.com>
2026-02-03 09:31:32 +0100

    db: Add schema script

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE education (
+    id SERIAL PRIMARY KEY,
+    level VARCHAR(20)
+);
+
+CREATE TABLE borrower (
+    id SERIAL PRIMARY KEY,
+    age SMALLINT,
+    education_id SMALLINT,
+    employment_duration SMALLINT,
+    residence_duraion SMALLINT,
+    income DECIMAL,
+    debt_income_ratio DECIMAL,
+    credit_debt DECIMAL,
+    other_debt DECIMAL,
+    total_debt DECIMAL,
+    is_default BOOLEAN,
+
+    CONSTRAINT fk_education
+        FOREIGN KEY (education_id)
+        REFERENCES education (id)
+        ON DELETE CASCADE
+);
